b074072e14a2ce0f25a457572bcbdcb72eac2f5b
sonoria <moravery@gmail.com>
2022-07-25 10:14:26 +0300

    добавлен токен авторизации, подключен react

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import argon2 from 'argon2';
 import { validationResult } from 'express-validator';
+import jsonwebtoken from 'jsonwebtoken';
+import config from 'config';
 import { signupValidator } from '../validation.js';
 import User from '../models/User.js';
 
 export const router = Router();
 
 const argon = argon2;
+const jwt = jsonwebtoken;
 
 // /account/signup
 router.post('/signup', signupValidator, async (req, res) => {
@@ -80,6 +83,12 @@ router.post('/signin', async (req, res) => {
     if (!isMatch) {
       return res.status(400).json({ message: 'Неверный пароль' });
     }
+
+    const token = jwt.sign({ userId: user.id }, config.get('jwtSecret'), {
+      expiresIn: '1h',
+    });
+
+    res.json({ token, userId: user.id, email: user.email });
 
     return res.status(200).json({ message: 'Авторизация прошла успешно!' });
   } catch (err) {
